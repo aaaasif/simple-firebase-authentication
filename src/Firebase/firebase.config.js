f2746807450fb7ccba65b8327bdde0ebf8f3a12c
aaaasif <alexasif4@gmail.com>
2021-10-08 11:23:11 +0600

    js added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZltFI0KwuezDSg_WwMAI7n4zYlfNrDaQ",
+    authDomain: "simple-firebase-authenti-42370.firebaseapp.com",
+    projectId: "simple-firebase-authenti-42370",
+    storageBucket: "simple-firebase-authenti-42370.appspot.com",
+    messagingSenderId: "998138411324",
+    appId: "1:998138411324:web:af995f2e1e6075844b4639"
+  };
+
+  export default firebaseConfig;
