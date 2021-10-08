2a44ab32496ef9e7e647fbdc7fb479e49f713aa0
aaaasif <alexasif4@gmail.com>
2021-10-08 11:44:05 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {GoogleAuthProvider} from 'firebase/auth';
+import {getAuth, signInWithProvider, GoogleAuthProvider} from 'firebase/auth';
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialize';
 
@@ -7,10 +7,15 @@ initializeAuthentication();
 
 const provider = new GoogleAuthProvider();
 
+const handleGoogleSignIn = () => {
+  const auth = Getauth();
+  signInWithProvider (auth, provider)
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
